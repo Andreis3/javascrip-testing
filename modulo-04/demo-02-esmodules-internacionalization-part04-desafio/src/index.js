cb10c4efa64fa138230654895d6a679e7da47e0c
Andreis3 <andrei.as3@hotmail.com>
2021-04-14 07:35:32 -0300

    criado modulo desafio

---
+++ modulo-04/demo-02-esmodules-internacionalization-part04-desafio/src/index.js
@@ -0,0 +1,35 @@
+import database from "../database.json";
+import TerminalController from './terminalController.js'
+import Person from './person.js';
+import { save } from './repository.js';
+
+const DEFAULT_LANG = 'pt-BR';
+const STOP_TERMINAL = ':q';
+
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question('What??');
+
+    if(answer === STOP_TERMINAL) {
+      terminalController.closeTerminal();
+      console.log('process finished!!!');
+      return;
+    }
+
+    // 2 Bike,Avião,Navio 200000000 2000-01-01 2002-02-01
+    const person = Person.generateInstanceFromString(answer);
+    terminalController.updateTable(person.formatted(DEFAULT_LANG));
+    await save(person);
+        
+    return mainLoop();
+
+  } catch (error) {
+    console.error('DEU RUIM**',error);
+    return mainLoop();
+  }
+}
+
+await mainLoop()
